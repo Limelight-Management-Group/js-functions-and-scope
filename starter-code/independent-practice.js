96498b04025884566da36458e9a3cf5d47a46524
Wayne Banks <thestorerun@yahoo.com>
2016-05-04 21:27:30 -0700

    time to start the hw =(

--- starter-code/independent-practice.js
+++ starter-code/independent-practice.js
@@ -4,6 +4,16 @@
   number is the length of the corresponding string.
 */
 
+function lengths(Array) {
+  var newArray = [];
+  for (var i = 0; i < array.length; i++) {
+    newArray.push(Array[i].length);
+  };
+  return newArray;
+};
+
+   for ( nunumbersArray.length;
+}
 // your code here
 
 /*
@@ -14,11 +24,25 @@
   Your function should return the "transmogrified" result.
 */
 
+function transmogrifier(num1, num2, num3) {
+  var Num1AndTwo = num1 * num2;
+  var transmogrified = Math.pow(Num1AndTwo, num3);
+ return transmogrified;
+};
+
+undefined
+transmogrifier(1, 2, 3);
+8
 // your code here
 
 /*
-  3. Write a function `wordReverse` that accepts a single argument, a string. The method should return a string with the order of the words (not letters) reversed. Don't worry
+  3. Write a function `wordReverse` that accepts a single argument, a string.
+  The method should return a string with the order of the words (not letters) reversed. Don't worry
   about punctuation. I.e. "hello world" should become "world hello".
 */
-
+function wordReverse(String) {
+  var words = String.split(' ');
+  var reversedWords = words.reverse();
+  return reversedWords.join(' ');
+};
 // your code here
